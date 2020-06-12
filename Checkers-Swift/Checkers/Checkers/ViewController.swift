0d29871c345f3260983879b4b8b1d13ad74fbc4f
michal1domanski <michal.domanski22@gmail.com>
2020-06-12 02:15:01 +0200

    initial commit

---
+++ Checkers-Swift/Checkers/Checkers/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Checkers
+//
+//  Created by Michał Domański on 12/06/2020.
+//  Copyright © 2020 Michał Domański. All rights reserved.
+//
+
+import Cocoa
+import SpriteKit
+import GameplayKit
+
+class ViewController: NSViewController {
+
+    @IBOutlet var skView: SKView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        if let view = self.skView {
+            // Load the SKScene from 'GameScene.sks'
+            if let scene = SKScene(fileNamed: "GameScene") {
+                // Set the scale mode to scale to fit the window
+                scene.scaleMode = .aspectFill
+                
+                // Present the scene
+                view.presentScene(scene)
+            }
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
+
