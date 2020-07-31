326ac345f9946e021ed0d39057c07942562d0205
michal1domanski <michal.domanski22@gmail.com>
2020-07-31 17:59:40 +0200

    added capturing pieces

--- Python/checkers-pygame.py
+++ Python/checkers-pygame.py
@@ -1,11 +1,19 @@
 import pygame
 import math
 
+'''
+do dodania:
+- damka (tworzenie i ruchy)
+- wymuszenie bicia (kiedy bicie jest dostępne musi zostać wykonane. jeśli bicie jednego pionka jest dostępne z wielu pozycji mozna wybrać dowolne z nich)
+'''
+
 BIALY = (255, 255, 255)
 CZARNY = (0, 0, 0)
 CZERWONY = (255, 0, 0)
 NIEBIESKI = (0, 0, 255)
 PODSWIETL = (0, 255, 0)
+NIEBIESKA_DAMA = (0, 125, 255)
+CZERWONA_DAMA = (255, 125, 0)
 
 class Gra:
 	def __init__(self):
@@ -14,6 +22,8 @@ class Gra:
 		self.tura = NIEBIESKI
 		self.wybrany_pionek = None
 		self.wybrany_mozliwy_ruch = []
+		self.bicie = False
+		self.bicie_macierz = []
 
 	def setup(self):
 		self.grafika.setup()
@@ -32,17 +42,32 @@ class Gra:
 		self.pozycja_myszy = self.grafika.koordynaty_planszy(pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1])
 		if self.wybrany_pionek != None:
 			self.wybrany_mozliwy_ruch = self.plansza.mozliwe_ruchy(self.pozycja_myszy[0], self.pozycja_myszy[1])
-
+		
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				pygame.quit()
 			if event.type == pygame.MOUSEBUTTONDOWN:
-				if self.plansza.matrix[self.pozycja_myszy[0]][self.pozycja_myszy[1]].zajecie != None and self.plansza.matrix[self.pozycja_myszy[0]][self.pozycja_myszy[1]].zajecie.kolor == self.tura:
-					self.wybrany_pionek = self.pozycja_myszy
-				elif self.wybrany_pionek != None and list(self.pozycja_myszy) in self.plansza.mozliwe_ruchy(self.wybrany_pionek[0],self.wybrany_pionek[1]):
-					if (self.wybrany_pionek[1] > self.pozycja_myszy[1] and self.plansza.matrix[self.wybrany_pionek[0]][self.wybrany_pionek[1]].zajecie.kolor == NIEBIESKI) or (self.wybrany_pionek[1] < self.pozycja_myszy[1] and self.plansza.matrix[self.wybrany_pionek[0]][self.wybrany_pionek[1]].zajecie.kolor == CZERWONY): #określenie kierunku poruszania się
-						self.plansza.rusz_pionek(self.wybrany_pionek[0], self.wybrany_pionek[1], self.pozycja_myszy[0], self.pozycja_myszy[1])
-						self.koniec_tury()
+				if self.bicie == False:
+					if self.plansza.matrix[self.pozycja_myszy[0]][self.pozycja_myszy[1]].zajecie != None and self.plansza.matrix[self.pozycja_myszy[0]][self.pozycja_myszy[1]].zajecie.kolor == self.tura:
+						self.wybrany_pionek = self.pozycja_myszy
+						self.bicie_macierz = self.plansza.bicie(self.pozycja_myszy[0], self.pozycja_myszy[1])
+						if self.bicie_macierz != []:
+							self.bicie = True
+
+					elif self.wybrany_pionek != None and list(self.pozycja_myszy) in self.plansza.mozliwe_ruchy(self.wybrany_pionek[0],self.wybrany_pionek[1]):
+
+						if (self.wybrany_pionek[1] > self.pozycja_myszy[1] and self.plansza.matrix[self.wybrany_pionek[0]][self.wybrany_pionek[1]].zajecie.kolor == NIEBIESKI) or (self.wybrany_pionek[1] < self.pozycja_myszy[1] and self.plansza.matrix[self.wybrany_pionek[0]][self.wybrany_pionek[1]].zajecie.kolor == CZERWONY): #określenie kierunku poruszania się
+							self.plansza.rusz_pionek(self.wybrany_pionek[0], self.wybrany_pionek[1], self.pozycja_myszy[0], self.pozycja_myszy[1])
+							self.koniec_tury()
+
+				if self.bicie == True:
+					if list(self.pozycja_myszy) in self.plansza.bicie(self.wybrany_pionek[0], self.wybrany_pionek[1]):
+						self.plansza.rusz_pionek(self.wybrany_pionek[0], self.wybrany_pionek[1],self.pozycja_myszy[0], self.pozycja_myszy[1])
+						self.plansza.usun_pionek(math.floor((self.wybrany_pionek[0] + self.pozycja_myszy[0])/2), math.floor((self.wybrany_pionek[1] + self.pozycja_myszy[1])/2))
+						if self.plansza.bicie(self.pozycja_myszy[0], self.pozycja_myszy[1]) == []:
+							self.koniec_tury()
+						else:
+							self.wybrany_pionek = self.pozycja_myszy
 
 	def koniec_tury(self):
 		if self.tura == NIEBIESKI:
@@ -52,6 +77,8 @@ class Gra:
 		
 		self.wybrany_pionek = None
 		self.wybrany_mozliwy_ruch = []
+		self.bicie = False
+		self.bicie_macierz = []
 		print("zmiana tury")
 
 class Grafika:
@@ -92,17 +119,20 @@ class Grafika:
 		return (koordynaty_planszy[0] * self.wielkosc_pola + self.promien_pionka, koordynaty_planszy[1] * self.wielkosc_pola +self.promien_pionka)
 
 	def pokaz_ruchy(self, koordynaty, plansza):
-		self.podswietl = [] #self.plansza.mozliwe_ruchy(koordynaty)
+		self.podswietl = [] 
 		self.mozliwe_ruchy = plansza.mozliwe_ruchy(koordynaty[0], koordynaty[1])
-		if plansza.matrix[koordynaty[0]][koordynaty[1]].zajecie.dama == False:
-			if plansza.matrix[koordynaty[0]][koordynaty[1]].zajecie.kolor == CZERWONY:
-				for i in range(len(self.mozliwe_ruchy)):
-					if self.mozliwe_ruchy[i][1] > koordynaty[1]:
-						self.podswietl.append(self.mozliwe_ruchy[i])
-			elif plansza.matrix[koordynaty[0]][koordynaty[1]].zajecie.kolor == NIEBIESKI:
-				for i in range(len(self.mozliwe_ruchy)):
-					if self.mozliwe_ruchy[i][1] < koordynaty[1]:
-						self.podswietl.append(self.mozliwe_ruchy[i])
+		if plansza.bicie(koordynaty[0], koordynaty[1]) == []:
+			if plansza.matrix[koordynaty[0]][koordynaty[1]].zajecie.dama == False:
+				if plansza.matrix[koordynaty[0]][koordynaty[1]].zajecie.kolor == CZERWONY:
+					for i in range(len(self.mozliwe_ruchy)):
+						if self.mozliwe_ruchy[i][1] > koordynaty[1]:
+							self.podswietl.append(self.mozliwe_ruchy[i])
+				elif plansza.matrix[koordynaty[0]][koordynaty[1]].zajecie.kolor == NIEBIESKI:
+					for i in range(len(self.mozliwe_ruchy)):
+						if self.mozliwe_ruchy[i][1] < koordynaty[1]:
+							self.podswietl.append(self.mozliwe_ruchy[i])
+		else:
+			self.podswietl = plansza.bicie(koordynaty[0], koordynaty[1])
 		for i in range(len(self.podswietl)):
 			pygame.draw.rect(self.screen, PODSWIETL, (self.podswietl[i][0] * self.wielkosc_pola, self.podswietl[i][1] * self.wielkosc_pola, self.wielkosc_pola, self.wielkosc_pola))
 
@@ -224,6 +254,48 @@ class Plansza:
 		if self.matrix[x][y].zajecie != None:
 			if (self.matrix[x][y].zajecie.kolor == NIEBIESKI and y == 0) or (self.matrix[x][y].zajecie.kolor == CZERWONY and y == 7):
 				self.matrix[x][y].zajecie.dama == True
+
+	def bicie(self, x, y):
+		self.gracz = self.matrix[x][y].zajecie.kolor
+		self.mozliwe_bicie = []
+		for i in range(len(self.pola_obok(x,y))):
+			if hasattr(self.matrix[self.pola_obok(x,y)[i][0]][self.pola_obok(x,y)[i][1]].zajecie, "kolor") == True:
+				if self.matrix[self.pola_obok(x,y)[i][0]][self.pola_obok(x,y)[i][1]].zajecie.kolor != self.gracz:
+					self.wrog = [self.pola_obok(x,y)[i][0],self.pola_obok(x,y)[i][1]]
+					self.linia = [(x - self.wrog[0])*2, (y - self.wrog[1])*2]
+					self.ruch_bicia = [x - self.linia[0], y - self.linia[1]]
+					if (self.ruch_bicia[0] <= 7 and self.ruch_bicia[0] >= 0) and (self.ruch_bicia[1] <= 7 and self.ruch_bicia[1] >= 0):
+						if self.matrix[self.ruch_bicia[0]][self.ruch_bicia[1]].zajecie == None:
+							self.mozliwe_bicie.append([x-self.linia[0], y-self.linia[1]])
+		return self.mozliwe_bicie
+
+	def pola_obok(self, x, y):
+		pola = []
+		if self.end_side_tile(y) == 1: #góra
+			if self.end_top_tile(x) == 1: #lewo
+				pola.append([x+1, y+1])
+			elif self.end_top_tile(x) == 0: #środek
+				pola.append([x+1, y+1])
+				pola.append([x-1, y+1])
+		elif self.end_side_tile(y) == 2: #dół
+			if self.end_top_tile(x) == 2: #prawo
+				pola.append([x-1, y-1])				
+			elif self.end_top_tile(x) == 0: #środek
+				pola.append([x+1, y-1])
+				pola.append([x-1, y-1])
+		elif self.end_side_tile(y) == 0: #środek
+			if self.end_top_tile(x) == 1:#lewo
+				pola.append([x+1, y+1])
+				pola.append([x+1, y-1])
+			elif self.end_top_tile(x) == 2:#prawo
+				pola.append([x-1, y+1])
+				pola.append([x-1, y-1])
+			elif self.end_top_tile(x) == 0: #środek
+				pola.append([x-1, y+1])
+				pola.append([x-1, y-1])
+				pola.append([x+1, y+1])
+				pola.append([x+1, y-1])
+		return pola
 
 class Pionek:
 	def __init__(self, kolor, dama = False):
